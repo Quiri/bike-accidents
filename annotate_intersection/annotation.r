eec4008edc04dbe2a1602e90f1a0fe21116f04b9
Andreas C. Osowski <andreas.chr.osowski@gmail.com>
2015-10-24 16:03:00 +0200

    Calculate nearest intersection

--- annotate_intersection/annotation.r
+++ annotate_intersection/annotation.r
@@ -1,4 +1,21 @@
-accidents <- read.csv('berlin_bike_accidents_neukoelln_2002_2015.csv');
-intersections <- read.csv('re_vms_detailnetz.csv');
+library(rgdal) 
+library(rgeos)
 
+accidents <- read.csv('berlin_bike_accidents_neukoelln_2002_2015.csv')
+intersections <- read.csv('re_vms_detailnetz.csv')
 
+coordinates(accidents) <- c("long", "lat")
+coordinates(intersections) <- c("x_wks84", "y_wks84")
+
+proj4string(accidents) <- CRS("+init=epsg:4326")
+proj4string(intersections) <- CRS("+init=epsg:4326")
+
+# WGS84
+CRS.new <- CRS("+init=epsg:4326")
+
+accidents.coords <- spTransform(accidents, CRS.new)
+intersections.coords <- spTransform(intersections, CRS.new)
+
+accidentsSp <- SpatialPoints(accidents.coords)
+intersectionsSp<- SpatialPoints(intersections.coords)
+accidents$nearest_intersection <- apply(gDistance(accidentsSp, intersectionsSp, byid=TRUE), 1, which.min)
