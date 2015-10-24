5a79a0e058280c0dda0c10ebbfe539c180f849af
Kirill Pomogajko <pomogajko@gmail.com>
2015-10-24 13:34:44 +0200

    make data path relative

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 # allzips <- readRDS("data/superzip.rds")
-allzips <- read.csv("/Users/mbrtargetinggmbh/Downloads/berlin_bike_accidents_neukoelln_2002_2015.csv")
+allzips <- read.csv("berlin_bike_accidents_neukoelln_2002_2015.csv")
 allzips$latitude <- jitter(allzips$lat)
 allzips$longitude <- jitter(allzips$long)
 allzips$college <- allzips$B2ALTER * 100
