2ee8a9cfd83f43d9a01012e9f26a1f39d014f790
Yuczyk <yuki@mbr-targeting.com>
2015-10-24 13:26:45 +0200

    simplified and added day of week selector

--- global.R
+++ global.R
@@ -1,23 +1,26 @@
 library(dplyr)
 
-allzips <- readRDS("data/superzip.rds")
-allzips$latitude <- jitter(allzips$latitude)
-allzips$longitude <- jitter(allzips$longitude)
-allzips$college <- allzips$college * 100
-allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
-row.names(allzips) <- allzips$zipcode
+# allzips <- readRDS("data/superzip.rds")
+allzips <- read.csv("/Users/mbrtargetinggmbh/Downloads/berlin_bike_accidents_neukoelln_2002_2015.csv")
+allzips$latitude <- jitter(allzips$lat)
+allzips$longitude <- jitter(allzips$long)
+allzips$college <- allzips$B2ALTER * 100
+# allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
+# row.names(allzips) <- allzips$zipcode
 
-cleantable <- allzips %>%
-  select(
-    City = city.x,
-    State = state.x,
-    Zipcode = zipcode,
-    Rank = rank,
-    Score = centile,
-    Superzip = superzip,
-    Population = adultpop,
-    College = college,
-    Income = income,
-    Lat = latitude,
-    Long = longitude
-  )
+# cleantable <- allzips %>%
+#   select(
+#     City = city.x,
+#     State = state.x,
+#     Zipcode = zipcode,
+#     Rank = rank,
+#     Score = centile,
+#     Superzip = superzip,
+#     Population = adultpop,
+#     College = college,
+#     Income = income,
+#     Lat = latitude,
+#     Long = longitude
+#   )
+
+cleantable <- allzips 
