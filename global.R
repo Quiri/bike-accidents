483ede31c7f7b5b81f32d487278ac1dfec16b0e7
Kirill Pomogajko <pomogajko@gmail.com>
2015-10-24 13:36:08 +0200

    Update global.R

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 # allzips <- readRDS("data/superzip.rds")
-allzips <- read.csv("berlin_bike_accidents_neukoelln_2002_2015.csv")
+allzips <- read.csv("data/berlin_bike_accidents_neukoelln_2002_2015.csv")
 allzips$latitude <- jitter(allzips$lat)
 allzips$longitude <- jitter(allzips$long)
 allzips$college <- allzips$B2ALTER * 100
